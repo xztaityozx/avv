db8133b3264060fd1eacd20281810818a3bc747d
xztaityozx <taityonohibi@gmail.com>
2019-01-23 22:03:38 +0900

    Add: version sub command

--- cmd/transistor.go
+++ cmd/transistor.go
@@ -1,5 +1,7 @@
 package cmd
 
+import "fmt"
+
 type Transistor struct {
 	Threshold float64
 	Sigma     float64
@@ -9,4 +11,8 @@ type Transistor struct {
 // Compare func for Transistor struct
 func (t Transistor) Compare(s Transistor) bool {
 	return t.Sigma == s.Sigma && t.Deviation == s.Deviation && t.Threshold == s.Threshold
+}
+
+func (t Transistor) ToString(prefix string) string {
+	return fmt.Sprintf("prefix%.4f-Sigma%.4f", t.Threshold, t.Sigma)
 }
