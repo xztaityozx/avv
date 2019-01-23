d7c6d9639cebb7cdb6295f673b58a99b1e92df13
xztaityozx <taityonohibi@gmail.com>
2019-01-23 17:23:11 +0900

    Test: テストこわれてます

--- cmd/resultsxml_test.go
+++ cmd/resultsxml_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/mitchellh/go-homedir"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,4 +11,21 @@ func TestResultsXML_MakeSweepFileObjects(t *testing.T) {
 	task := Task{
 		Times:100,
 	}
+
+	home, _ := homedir.Dir()
+	task.SimulationDirectories.DstDir=PathJoin(home,"Dst")
+	FU.TryMkDir(PathJoin(home,"Dsr"))
+	task.SimulationDirectories.NetListDir=PathJoin(home,"NetList")
+	FU.TryMkDir(task.SimulationDirectories.NetListDir)
+
+	path, err := task.NewResultsXml()
+	if err != nil {
+		as.Error(err)
+	}
+
+	expect := FU.Cat("../test/100.xml")
+	actual := FU.Cat(path)
+
+	as.Equal(expect,actual)
+
 }
