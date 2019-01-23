1dee7d6dcbb0215eccab0bb8bf9d735f8be23165
xztaityozx <taityonohibi@gmail.com>
2019-01-23 17:02:11 +0900

    Add: resultsxml

---
+++ cmd/resultsxml_test.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestResultsXML_MakeSweepFileObjects(t *testing.T) {
+	as := assert.New(t)
+	task := Task{
+		Times:100,
+	}
+}
