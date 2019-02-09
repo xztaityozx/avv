4acfad17e124a2022ec637fac3f12828714da4cc
xztaityozx <taityonohibi@gmail.com>
2019-02-10 01:26:40 +0900

    Add: resultRecord Test

--- cmd/resultrecord.go
+++ cmd/resultrecord.go
@@ -1,18 +1,52 @@
 package cmd
 
 import (
+	"context"
 	"time"
 )
 
 type (
 	ResultRecord struct {
-		Id      int
+		Id      int64
 		TaskId  int64
 		Seed    int64
 		Failure int64
 		Date    time.Time
 	}
 )
+
+func (ResultRecord) InsertQuery() string {
+	return "insert into Results(TaskId, Seed, Failure, Date) values (?,?,?,?)"
+}
+
+func (rr ResultRecord) Select(ctx context.Context, r Repository) ([]ResultRecord, error) {
+	var rt []ResultRecord
+
+	db, err := r.Connect()
+	defer db.Db.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.WithContext(ctx).Select(&rt,rr.SelectQuery(),rr.TaskId)
+
+	return rt, nil
+}
+
+func (rr ResultRecord) Insert(ctx context.Context, repository Repository) error {
+	db, err :=repository.Connect()
+	defer db.Db.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = db.WithContext(ctx).Exec(rr.InsertQuery(), rr.TaskId,rr.Seed,rr.Failure,rr.Date)
+	return err
+}
+
+func (ResultRecord) SelectQuery() string {
+	return "select * from Results where TaskId = ?"
+}
 
 // NewResultRecord create ResultRecord struct
 // args: succeeded TaskResult
@@ -25,3 +59,4 @@ func NewResultRecord(result CountResult) ResultRecord {
 		Failure:result.Failure,
 	}
 }
+
--- cmd/resultrecord_test.go
+++ cmd/resultrecord_test.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"context"
+	"github.com/mitchellh/go-homedir"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -15,4 +18,50 @@ func TestNewResultRecord(t *testing.T) {
 	as.Equal(20, actual.Failure)
 	as.Equal(1, actual.TaskId)
 	as.Equal(10,actual.Seed)
+}
+
+func TestResultRecord_InsertQuery(t *testing.T) {
+	as:=assert.New(t)
+	expect := "insert into Results(TaskId, Seed, Failure, Date) values (?,?,?,?)"
+	actual := ResultRecord{}.InsertQuery()
+
+	as.Equal(expect,actual)
+}
+
+func TestResultRecord_SelectQuery(t *testing.T) {
+	expect := "select * from Results where TaskId = ?"
+	actual := ResultRecord{}.SelectQuery()
+	assert.Equal(t,expect,actual)
+}
+
+func TestResultRecord_Insert(t *testing.T) {
+	as := assert.New(t)
+	home, _ := homedir.Dir()
+	p := PathJoin(home,"TestDir","db")
+	FU.TryMkDir(p)
+	p=PathJoin(p,"database.db")
+
+	os.Remove(p)
+
+	r:=NewRepositoryFromFile(p)
+
+	rr := NewResultRecord(CountResult{
+		Failure:10,
+		Task:Task{
+			TaskId:50,
+			SEED:20,
+			Repository:r,
+		}})
+	err := rr.Insert(context.Background(),r)
+	as.NoError(err)
+
+	t.Run("Check_Select", func(t *testing.T) {
+		res, err := rr.Select(context.Background(),r)
+		as.NoError(err)
+		as.Equal(1,len(res))
+		as.Equal(int64(1), res[0].Id)
+		as.Equal(int64(50),res[0].TaskId)
+		as.Equal(int64(20),res[0].Seed)
+		as.Equal(int64(10), res[0].Failure)
+	})
 }
