9ec91af79d4950d2a5ac50ea4d9bafbfa8f7ceb0
xztaityozx <taityonohibi@gmail.com>
2019-04-18 14:33:11 +0900

    update

--- cmd/plotpoint_test.go
+++ cmd/plotpoint_test.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"github.com/mitchellh/go-homedir"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAllPlotPoint(t *testing.T) {
@@ -76,4 +77,21 @@ func TestPlotPoint_GetAwkScript(t *testing.T) {
 	expect := "BEGIN{sum=0}$1>=1&&$2<=6&&$3>1&&$4>=2&&$5<=7&&$6>2&&$7>=3&&$8<=8&&$9>3&&$10>=4&&$11<=9&&$12>4&&$13>=5&&$14<=0&&$15>5{sum++}END{print sum}"
 
 	assert.Equal(t, expect, actual)
+}
+
+func TestPlotpoint_ToFilterString(t *testing.T) {
+	pp := PlotPoint{
+		Filters: []Filter{
+			{SignalName: "A", Status: []string{">=:1", "<=:6", ">:1"}},
+		},
+		Start: 2.5,
+		Step:  7.5,
+		Stop:  17.5,
+	}
+
+	actual, err := pp.ToFilterStrings()
+	assert.NoError(t, err)
+
+	assert.Equal(t, "2.5n:>=:1 10n:<=:6 17.5n:>:1", actual)
+
 }
