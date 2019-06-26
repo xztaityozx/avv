d81183ea31303a6aa064ab157b89914b85da8b69
xztaityozx <taityonohibi@gmail.com>
2019-06-26 14:41:20 +0900

    Tested: simulation package

--- push/push_test.go
+++ push/push_test.go
@@ -1,8 +1,51 @@
 package push
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/xztaityozx/avv/parameters"
+	"github.com/xztaityozx/avv/task"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
-// TODO: Implement test
 func TestTaa_Invoke(t *testing.T) {
+	gohome := os.Getenv("GOPATH")
+	taa := Taa{
+		TaaPath: filepath.Join(gohome, "src", "github.com", "xztaityozx", "avv", "test", "taa.exe"),
+	}
 
+	x := task.Task{
+		Files: parameters.Files{
+			ResultFile: "/path/to/SEED00001.csv",
+		},
+	}
+
+	y := task.Task{
+		Files: parameters.Files{
+			ResultFile: "/path/to/XXXX.csv",
+		},
+	}
+
+	as := assert.New(t)
+
+	ctx := context.Background()
+
+	t.Run("unexpected", func(t *testing.T) {
+		_, err := taa.Invoke(ctx, y)
+		as.Error(err)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		taa.ConfigFile = "err"
+		_, err := taa.Invoke(ctx, x)
+		as.Error(err)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		taa.ConfigFile = "/path/to/config"
+		_, err := taa.Invoke(ctx, x)
+		as.NoError(err)
+	})
 }
--- simulation/simulate_test.go
+++ simulation/simulate_test.go
@@ -1,7 +1,38 @@
 package simulation
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/xztaityozx/avv/parameters"
+	"github.com/xztaityozx/avv/task"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestHSPICE_Invoke(t *testing.T) {
+	gopath := os.Getenv("GOPATH")
+	h := HSPICE{
+		Path: filepath.Join(gopath, "src", "github.com", "xztaityozx", "avv", "test", "hspice.sh"),
+	}
 
+	x := task.Task{
+		Parameters: parameters.Parameters{
+			Seed: 1,
+		},
+	}
+
+	as := assert.New(t)
+
+	t.Run("fail", func(t *testing.T) {
+		h.Options = "err"
+		_, err := h.Invoke(context.Background(), x)
+		as.Error(err)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		h.Options = ""
+		_, err := h.Invoke(context.Background(), x)
+		as.NoError(err)
+	})
 }
